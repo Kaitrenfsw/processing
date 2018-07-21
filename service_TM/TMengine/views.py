0eeb8b7af7d90aaadb9fa45de9466d03ad1a9431
SimonContreras <simon.contreras.13@sansano.usm.cl>
2018-07-21 01:01:44 -0400

    Update method for ldamodel, reformulated, models comparator and topic selector added

--- service_TM/TMengine/views.py
+++ service_TM/TMengine/views.py
@@ -1,6 +1,7 @@
+from topic.models import Topic, Keyword
+from TMengine.models import LdaModel
 from new.models import New
-from topic.serializers import TopicSerializer, KeywordSerializer
-from TMengine.engine_trainer import update_or_create_newest_model
+from TMengine.engine_trainer import update_model
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
@@ -13,17 +14,7 @@ class LdaModelViewSet(viewsets.ViewSet):
 
     @staticmethod
     def create(request):
-        data = request.data
-        try:
-            news = list(New.objects.all().values_list('full_text', flat=True))
-            new_name = update_or_create_newest_model(data["action"], news)
-            response_message = {"New Model created successfully!, new filename: " + new_name}
-            status_message = status.HTTP_200_OK
-        except Exception as e:
-            response_message = {e}
-            status_message = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return Response(data=response_message,
-                        status=status_message)
+        return Response(data={":)"})
 
     @staticmethod
     def retrieve(request, pk=None):
@@ -31,11 +22,20 @@ class LdaModelViewSet(viewsets.ViewSet):
 
     @staticmethod
     def update(request, pk=None):
-        data = request.data
         try:
-            news = list(New.objects.all().values_list('full_text', flat=True))
-            new_name = update_or_create_newest_model(data["action"], news)
-            response_message = {"Model updated successfully!, new filename: " + new_name}
+            news = list(New.objects.all().values_list('text', flat=True))
+            new_filename, new_topics = update_model(news)
+            ldamodel_instance = LdaModel.objects.get(filename=new_filename)
+            for topic in new_topics:
+                topic_instance = Topic(topic_number=topic['topic_number'],
+                                       lda_model=ldamodel_instance)
+                topic_instance.save()
+                for keyword in topic["keywords"]:
+                    keyword_instance = Keyword(name=keyword['name'],
+                                               weight=keyword['weight'],
+                                               topic_id=topic_instance)
+                    keyword_instance.save()
+            response_message = {"Model updated successfully!, new filename: " + new_filename}
             status_message = status.HTTP_200_OK
         except Exception as e:
             response_message = {e}
